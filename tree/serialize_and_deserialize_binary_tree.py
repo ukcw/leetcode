ac436276c7f9da816bf60fa1efa85a50401c93a3
Ulysses Kee <ulysseskcw96@gmail.com>
2022-08-09 14:54:37 +0800

    Add DFS solution

--- tree/serialize_and_deserialize_binary_tree.py
+++ tree/serialize_and_deserialize_binary_tree.py
@@ -50,6 +50,47 @@ class BFSCodec:
 
         return root
 
+class DFSCodec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+        output = []
+        stack = [root]
+
+        while stack:
+            node = stack.pop()
+            if node:
+                output.append(str(node.val))
+                stack.append(node.right)
+                stack.append(node.left)
+            if not node:
+                output.append('#')
+
+        return ' '.join(output)
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        def createNode(val):
+            return None if val == '#' else TreeNode(int(val))
+
+        self.data = collections.deque(data.split())
+        def dfs():
+            root = createNode(self.data.popleft())
+            if root:
+                root.left = dfs()
+                root.right = dfs()
+                return root
+
+        return dfs()
+
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
 # deser = Codec()
